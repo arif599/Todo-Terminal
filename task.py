82817c6d555960ed0fe919766a0880406666a36c
Arifulla Shaik <arifullashaik01@gmail.com>
2020-11-22 14:13:47 -0800

    added a new attribute completed with is bool (0 or 1)

--- task.py
+++ task.py
@@ -1,13 +1,14 @@
 # Blueprint for Task objects
 class Task:
     # constructor
-    def __init__(self, title, description, dueDate, createdDate, priority):
+    def __init__(self, title, description, dueDate, createdDate, priority, completed):
         # private variables
         self.set_title(title)
         self.set_description(description)
         self.set_dueDate(dueDate)
         self.set_createdDate(createdDate)
         self.set_priority(priority)
+        self.set_completed(completed)
 
     # getters
     def get_title(self):
@@ -24,6 +25,9 @@ class Task:
 
     def get_priority(self):
         return self._priority
+    
+    def get_completed(self):
+        return self._completed
 
 
     # setters
@@ -42,6 +46,9 @@ class Task:
     def set_priority(self, priority):
         self._priority = priority
 
+    def set_completed(self, completed):
+        self._completed = completed
+
 
 
     # methods
@@ -51,5 +58,9 @@ class Task:
         strCreatedDate = f"Created Date: {self.get_createdDate()}"
         strDueDate = f"Due Date: {self.get_dueDate()}"
         strPriority = f"Priority Level: {self.get_priority()}"
+        if self.get_completed() == 1:
+            strCompleted = f"This task is COMPLETED"
+        else:
+            strCompleted = f"This task is NOT COMPLETED"
 
-        return strTask + "\n" + strDescription + "\n" + strCreatedDate + "\n" + strDueDate + "\n" + strPriority
+        return strTask + "\n" + strDescription + "\n" + strCreatedDate + "\n" + strDueDate + "\n" + strPriority +"\n"+ strCompleted
